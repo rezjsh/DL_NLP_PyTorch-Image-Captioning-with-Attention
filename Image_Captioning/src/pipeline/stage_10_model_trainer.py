2d4e235461e10f54213d96e9cf98f69e57f60ee3
rezjsh <149146755+rezjsh@users.noreply.github.com>
2025-10-20 09:58:52 +0330

    Add model training pipeline stage for image captioning

---
+++ Image_Captioning/src/pipeline/stage_10_model_trainer.py
@@ -0,0 +1,37 @@
+from src.utils.logging_setup import logger
+from src.config.configuration import ConfigurationManager
+from src.components.model_trainer import Trainer
+from src.components.encoder_decoder import TransformerImageCaptioningModel 
+from src.components.text_preprocessing import TextPreprocessor 
+from torch.utils.data import DataLoader 
+
+class TrainingPipeline:
+    def __init__(self, config: ConfigurationManager):
+        self.config = config.get_model_trainer_config()
+
+    def run_pipeline(self, 
+                     model: TransformerImageCaptioningModel, 
+                     train_loader: DataLoader, 
+                     val_loader: DataLoader, 
+                     text_preprocessor: TextPreprocessor) -> None:
+        '''
+        Initializes and runs the training component.
+        
+        Args:
+            model (TransformerImageCaptioningModel): The final Encoder-Decoder model.
+            train_loader (DataLoader): The DataLoader for the training set.
+            val_loader (DataLoader): The DataLoader for the validation set.
+            text_preprocessor (TextPreprocessor): The initialized text preprocessor (for PAD index).
+        '''
+        logger.info("Trainer initialized. Starting training...")
+        
+        trainer = Trainer(
+            config=self.config, 
+            model=model, 
+            train_loader=train_loader, 
+            val_loader=val_loader,
+            text_preprocessor=text_preprocessor
+        )
+        
+        trainer.train()
+        logger.info("Training process completed.")
