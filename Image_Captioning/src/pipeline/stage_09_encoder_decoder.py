260dd1d1f6835b113ff89019fddfacd5c3eff76e
rezjsh <149146755+rezjsh@users.noreply.github.com>
2025-11-09 07:29:13 +0330

    Refactor EncoderDecoderPipeline to improve parameter handling and add error handling

--- Image_Captioning/src/pipeline/stage_09_encoder_decoder.py
+++ Image_Captioning/src/pipeline/stage_09_encoder_decoder.py
@@ -1,19 +1,25 @@
-
-from src.components.encoder import CNNTransformerEncoder
-from src.components.decoder import  TransformerDecoder
+# src/pipeline/stage_09_encoder_decoder.py
 from src.utils.logging_setup import logger
 from src.config.configuration import ConfigurationManager
 from src.components.encoder_decoder import TransformerImageCaptioningModel
+from src.components.encoder import CNNTransformerEncoder
+from src.components.decoder import TransformerDecoder
 
 class EncoderDecoderPipeline:
-    def __init__(self, config:ConfigurationManager, vocab_size:int, transformer_encoder: CNNTransformerEncoder, transformer_decoder: TransformerDecoder) -> None:
+    def __init__(self, config: ConfigurationManager):
         self.config = config.get_encoder_decoder_config()
-        self.vocab_size = vocab_size
-        self.encoder = transformer_encoder
-        self.decoder = transformer_decoder
 
-    def run_pipeline(self):
-        logger.info("Running Encoder-Decoder Pipeline")
-        model = TransformerImageCaptioningModel(config=self.config, vocab_size=self.vocab_size, transformer_encoder=self.encoder, transformer_decoder=self.decoder)
-        logger.info(f"Encoder-Decoder model created: {model}")
-        return model
+    def run_pipeline(self, vocab_size: int, transformer_encoder: CNNTransformerEncoder, transformer_decoder: TransformerDecoder) -> TransformerImageCaptioningModel:
+        try:
+            logger.info("Running Encoder-Decoder Model Pipeline...")
+            model = TransformerImageCaptioningModel(
+                config=self.config,
+                vocab_size=vocab_size,
+                transformer_encoder=transformer_encoder,
+                transformer_decoder=transformer_decoder
+            )
+            logger.info("Encoder-Decoder Model Pipeline completed.")
+            return model
+        except Exception as e:
+            logger.error(f"Error occurred in Encoder-Decoder Model Pipeline: {e}")
+            raise e
