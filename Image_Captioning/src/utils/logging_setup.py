c7dff4cbd3d83e59b35d4daf4c6e41f46d45c041
rezjsh <149146755+rezjsh@users.noreply.github.com>
2025-11-09 05:23:44 +0330

    Fix import path for singleton module in logging setup

--- Image_Captioning/src/utils/logging_setup.py
+++ Image_Captioning/src/utils/logging_setup.py
@@ -1,7 +1,7 @@
 import logging
 import os
 import sys
-from Image_Captioning.src.core.singleton import SingletonMeta
+from src.core.singleton import SingletonMeta
 
 class Logger(metaclass=SingletonMeta):
     """
