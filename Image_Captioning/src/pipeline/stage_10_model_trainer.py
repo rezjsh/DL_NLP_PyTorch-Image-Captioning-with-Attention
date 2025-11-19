0673999ea8b47580a38a71cb17c1da0086ab48c4
rezjsh <149146755+rezjsh@users.noreply.github.com>
2025-11-19 07:33:59 +0330

    Refactor TrainingPipeline class for improved code readability and formatting consistency

--- Image_Captioning/src/pipeline/stage_10_model_trainer.py
+++ Image_Captioning/src/pipeline/stage_10_model_trainer.py
@@ -1,22 +1,22 @@
 from src.utils.logging_setup import logger
 from src.config.configuration import ConfigurationManager
 from src.components.model_trainer import Trainer
-from src.components.encoder_decoder import TransformerImageCaptioningModel 
-from src.components.text_preprocessing import TextPreprocessor 
-from torch.utils.data import DataLoader 
+from src.components.encoder_decoder import TransformerImageCaptioningModel
+from src.components.text_preprocessing import TextPreprocessor
+from torch.utils.data import DataLoader
 
 class TrainingPipeline:
     def __init__(self, config: ConfigurationManager):
         self.config = config.get_model_trainer_config()
 
-    def run_pipeline(self, 
-                     model: TransformerImageCaptioningModel, 
-                     train_loader: DataLoader, 
-                     val_loader: DataLoader, 
+    def run_pipeline(self,
+                     model: TransformerImageCaptioningModel,
+                     train_loader: DataLoader,
+                     val_loader: DataLoader,
                      text_preprocessor: TextPreprocessor) -> None:
         '''
         Initializes and runs the training component.
-        
+
         Args:
             model (TransformerImageCaptioningModel): The final Encoder-Decoder model.
             train_loader (DataLoader): The DataLoader for the training set.
@@ -24,14 +24,15 @@ class TrainingPipeline:
             text_preprocessor (TextPreprocessor): The initialized text preprocessor (for PAD index).
         '''
         logger.info("Trainer initialized. Starting training...")
-        
+
+        # Instantiate and run the Trainer component
         trainer = Trainer(
-            config=self.config, 
-            model=model, 
-            train_loader=train_loader, 
-            val_loader=val_loader,
+            config=self.config,
+            model=model,
+            train_loader=train_loader,
+            val_loader=val_loader, # Pass the validation loader
             text_preprocessor=text_preprocessor
         )
-        
+
         trainer.train()
         logger.info("Training process completed.")
