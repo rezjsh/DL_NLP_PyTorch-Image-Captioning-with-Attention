91a8abe57e5e4b1f88d7204c5fcabfbeea0e33c6
rezjsh <149146755+rezjsh@users.noreply.github.com>
2025-11-19 07:38:43 +0330

    Add constants for allowed file extensions and upload folder in Flask app

--- Image_Captioning/src/constants/constants.py
+++ Image_Captioning/src/constants/constants.py
@@ -1,2 +1,7 @@
+#config paths
 CONFIG_FILE_PATH = "config/config.yaml"
 PARAMS_FILE_PATH = "params.yaml"
+
+#flask app
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+UPLOAD_FOLDER = 'app/static/uploads'
